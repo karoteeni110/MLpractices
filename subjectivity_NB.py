ba43f5d6d2069564b67251beab78b2210d856fef
karoteeni110 <30997769+karoteeni110@users.noreply.github.com>
2019-07-21 20:59:12 +0300

    organized

--- subjectivity_NB.py
+++ subjectivity_NB.py
@@ -1,4 +1,17 @@
-# Supervised learning: subjectivity
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Welcome to your notebook
+# 
+# Learn how to write your own cells containing Markdown text and Python code.
+
+# In[1]:
+
+
+# If you want to plot graphics in your notebook, keep this %matplotlib command here before your imports
+# %matplotlib notebook
+
+# Import some necessary modules
 import time
 from collections import Counter, defaultdict
 from random import shuffle
@@ -7,67 +20,149 @@ import nltk
 import numpy as np
 from nltk.corpus import stopwords
 from nltk.corpus import subjectivity as sub
-from sacremoses import MosesDetokenizer
+from nltk.stem import WordNetLemmatizer 
 
-# Lemmatizing
 
-# Filtering stop words 
+# In[ ]:
 
-# ~~~~~~~~~~
 
-def sent2feature(sent):
+
+
+
+# In[46]:
+
+
+def divide_sets(labeled_sents):
+    shuffle(labeled_sents)
+    ratio = int(len(labeled_sents) * 0.8)
+    train, test = labeled_sents[:ratio], labeled_sents[ratio:]
+    return train, test
+
+
+# In[91]:
+
+
+labeled_sents = [(sent, label) for label in sub.categories() for sent in sub.sents(categories=label)]
+trainset, testset = divide_sets(labeled_sents)
+
+
+# In[92]:
+
+
+print(trainset[0])
+print(testset[0])
+
+
+# In[93]:
+
+
+lemtzer = WordNetLemmatizer()
+stopws = stopwords.words('english')
+fd = nltk.FreqDist(lemtzer.lemmatize(w) for sent, label in trainset for w in sent
+                    if w.isalnum() and w not in stopws)
+ftwords = list(fd)[:500]
+
+def sent2ft(sent):
     features = {}
+    lemmasent = [lemtzer.lemmatize(w.lower()) for w in sent]
     for w in ftwords:
-        features['contains({})'.format(w)] = (w in sent)
+        features['contains({})'.format(w)] = (w in lemmasent)
     return features
 
-stopws = stopwords.words('english')
-fd = nltk.FreqDist(w.lower() for w in sub.words() if w not in stopws and w.isalpha())
-ftwords = set(list(fd)[:500])
-
-# print(sent2feature(sub.sents()[9]))
-
-# data = [(sent2feature(sent), label) for label in sub.categories()
-#                       for sent in sub.sents(categories=label)]
-# shuffle(data)
-# trainset, testset = data[:8000], data[8000:]
-
-def get_ftset(sentset):
-    return [(sent2feature(sent), label) for sent, label in sentset]
-
-labeled_sents = [(sent, label) for label in sub.categories()
-                    for sent in sub.sents(categories=label)]
-shuffle(labeled_sents)
-train_sents, test_sents = labeled_sents[:8000], labeled_sents[8000:]
-trainset, testset = get_ftset(train_sents), get_ftset(test_sents)
+def data2ftset(sentset):
+    return [(sent2ft(sent), label) for sent, label in sentset]
 
 
-# Result from NLTK built-in NB classifier
-def nltkResults():
-    # start = time.time()
-    classifier = nltk.NaiveBayesClassifier.train(trainset)
-    # print(nltk.classify.accuracy(classifier, testset))
-    MIFs = classifier.most_informative_features(20)
-    classifier.show_most_informative_features(5)
-    # end = time.time()
-    # print('Time:', end-start)
-    errlst = []
-    dt = MosesDetokenizer(lang='en')
-    for sent,gold_l in test_sents:
-        ft = sent2feature(sent)
-        pred_l = classifier.classify(ft)
-        probs = classifier.prob_classify(ft)
-        if pred_l != gold_l:
-            errlst.append([pred_l, sent, ft, probs])
-    print('Errs:', len(errlst))
-    ensi_20 = int(len(errlst) * 0.2)
-    for pred_l, sent, ftset, probs in errlst[:ensi_20]:
-        print('Wrong prediction: %s' % pred_l)
-        # print(('Subj %f VS Obj %f') % (probs['subj'], probs['obj']))
-        print(dt.detokenize(sent, return_str=True))
-        for ft in ftset:
-            if ft in MIFs:
-                print('Informative feature: %s:%s' % (ft,ftset[ft]))
+# In[94]:
+
+
+print(ftwords[:10])
+
+
+# In[95]:
+
+
+train_ftset, test_ftset = data2ftset(trainset), data2ftset(testset)
+
+
+# Transform the data (the tokenized sentences) into classifier recognizable formats, i.e. the features.
+# 
+# The feature here is whether the sentence contains each of the featured words (parameter ``ftwords``), which are picked from the 500 most frequent non-stop words in the training set.
+
+# In[96]:
+
+
+train_ftset[0]
+
+
+# In[ ]:
+
+
+
+
+
+# In[75]:
+
+
+
+
+
+# In[131]:
+
+
+clfNB = nltk.NaiveBayesClassifier.train(train_ftset)
+print('NB Accuracy: {:4.2f}'.format(nltk.classify.accuracy(clfNB, test_ftset))) 
+clfNB.show_most_informative_features(20)
+    
+# NBResults()
+
+
+# In[132]:
+
+
+def show_errcases(n_errcases, classifier, test_set):
+    errlst, i = [], 0
+    while len(errlst) <= n_errcases: # Get 10 wrongly classified sentences
+        sent, goldlabel = test_set[i]
+        sentft = sent2ft(sent)
+        testlabel = classifier.classify(sentft)
+        if testlabel != goldlabel:
+            errlst.append([testlabel, sent, sentft])
+        i += 1
+        
+    print('%s wrongly classified instances:' % n_errcases)
+    print()
+    
+    for wronglabel, sent, sentft in errlst:
+        print(sent)
+        print('Wrongly classified as: %s' % wronglabel)
+        informative_fts =  [(ft, sentft[ft]) for ft in sentft if sentft[ft] == True]
+        print('Informative features:', informative_fts)
         print()
+        
+show_errcases(10, clfNB, testset)
 
-nltkResults()
+
+# In[133]:
+
+
+def classifier_label_list(classifier, test_ftset):
+    testlst, goldlst = [], []
+    for ft, lb in test_ftset:
+        testlst.append(classifier.classify(ft))
+        goldlst.append(lb)
+    return testlst, goldlst
+
+def show_cm(classifier, test_ftset):
+    gold, test = classifier_label_list(classifier, test_ftset)
+    cm = nltk.ConfusionMatrix(gold, test)
+    print(cm.pretty_format(sort_by_count=True, show_percents=True))
+    
+show_cm(clfNB, test_ftset)
+
+
+# In[ ]:
+
+
+
+
