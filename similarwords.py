6a4a2a065c7bde604c9f3087bdc2fafd084ec1e5
karoteeni110 <akb48auy@sohu.com>
2019-08-13 17:46:05 +0300

    DONE

--- similarwords.py
+++ similarwords.py
@@ -1,11 +1,13 @@
 import nltk
 nltk.download('inaugural')
+nltk.download('wordnet')
 from nltk.corpus import inaugural, stopwords
 from nltk.util import ngrams 
 from collections import defaultdict, Counter
-from random import shuffle
+from random import shuffle, choice
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity, cosine_distances
+from nltk.stem import WordNetLemmatizer
 
 DIM = 10
 
@@ -26,10 +28,7 @@ def normalise(vec_dict):
         vec_dict[w] = vec_dict[w] / sum(vec_dict.values())
     return vec_dict
     
-def get_word_vecs(filtered_sents):
-    n_grams = get_ngrams(filtered_sents)
-    vocab = set(word for sent in filtered_sents for word in sent)
-
+def get_word_vecs():
     index_vec, context_vec = {}, {}
     for w in vocab:
         index_vec[w] = random_vec()
@@ -43,31 +42,38 @@ def get_word_vecs(filtered_sents):
             context_vec[focus] += index_vec[w]
     return normalise(context_vec)
 
-def word2idx(word):
-    return word2idx_dict[word]
-
-def mostsimilar(word, vec_dict, most = 3):
+def mostsimilar(word, most = 10):
     pairwise_sims = cosine_similarity(vec_dict[word].reshape(1,-1), list(vec_dict.values()))
     # print(pairwise_sims)
     maxargs = pairwise_sims[0].argsort()[most*(-1):][::-1]
     print('Most %d similar words for `%s`:' % (most, word))
+    print()
     print('Word', '          ', 'Cosine Similarity')
     for arg in maxargs:
         compared_word = list(vec_dict.keys())[arg] 
-        print(compared_word, ' ' * (14-len(compared_word)) , pairwise_sims[0][arg])
+        if compared_word != word:
+            print(compared_word, ' ' * (14-len(compared_word)) , pairwise_sims[0][arg])
     print()
 
+def random_similarwords(examples=10):
+    for i in range(examples):
+        word = choice(list(vec_dict.keys()))
+        mostsimilar(word)
 
 if __name__ == "__main__":
     # Get vocabulary & ngrams
     window_size = 5
     stopwords = stopwords.words('english')
-    filtered_sents = [tuple(word.lower() for word in sent if word.isalnum() and not word in stopwords) \
-                        for sent in inaugural.sents()[:10]]
-    vec_dict = get_word_vecs(filtered_sents)
-    word2idx_dict = {word:i for i, word in enumerate(vec_dict.keys())}
-    # a,b = vec_dict['good'], vec_dict['great']
-    # c = vec_dict['bad']
-    mostsimilar('good', vec_dict)
-    # print('Cosine distance between `good` and `great`:', cosine_similarity(a,b))
-    # print('Cosine distance between `good` and `bad`:', cosine_similarity(a,c))
+    lmtzer = WordNetLemmatizer()
+    filtered_sents = [tuple(lmtzer.lemmatize(word.lower()) \
+                    for word in sent if word.isalnum() and not word in stopwords) \
+                    for sent in inaugural.sents()]
+    n_grams = get_ngrams(filtered_sents)
+    vocab = set(word for sent in filtered_sents for word in sent)
+    vec_dict = get_word_vecs()
+
+    mostsimilar('good')
+
+    random_similarwords(examples=10)
+
+
