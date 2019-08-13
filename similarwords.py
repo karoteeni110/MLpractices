b6e8011494762d86a3c0424310581cb84095275a
karoteeni110 <akb48auy@sohu.com>
2019-08-13 17:22:44 +0300

    mostsimilar()

--- similarwords.py
+++ similarwords.py
@@ -5,9 +5,9 @@ from nltk.util import ngrams
 from collections import defaultdict, Counter
 from random import shuffle
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity, cosine_distances
 
-DIM = 100
+DIM = 10
 
 def random_vec(dim=DIM):
     vec = np.zeros(DIM, dtype=int)
@@ -43,22 +43,31 @@ def get_word_vecs(filtered_sents):
             context_vec[focus] += index_vec[w]
     return normalise(context_vec)
 
-def word2idx():
-    return vec_dict[word]
+def word2idx(word):
+    return word2idx_dict[word]
 
-def mostsimilar(word, vec_dict):
-    pass
+def mostsimilar(word, vec_dict, most = 3):
+    pairwise_sims = cosine_similarity(vec_dict[word].reshape(1,-1), list(vec_dict.values()))
+    # print(pairwise_sims)
+    maxargs = pairwise_sims[0].argsort()[most*(-1):][::-1]
+    print('Most %d similar words for `%s`:' % (most, word))
+    print('Word', '          ', 'Cosine Similarity')
+    for arg in maxargs:
+        compared_word = list(vec_dict.keys())[arg] 
+        print(compared_word, ' ' * (14-len(compared_word)) , pairwise_sims[0][arg])
+    print()
+
 
 if __name__ == "__main__":
     # Get vocabulary & ngrams
     window_size = 5
     stopwords = stopwords.words('english')
     filtered_sents = [tuple(word.lower() for word in sent if word.isalnum() and not word in stopwords) \
-                        for sent in inaugural.sents()[:20]]
+                        for sent in inaugural.sents()[:10]]
     vec_dict = get_word_vecs(filtered_sents)
-
+    word2idx_dict = {word:i for i, word in enumerate(vec_dict.keys())}
     # a,b = vec_dict['good'], vec_dict['great']
     # c = vec_dict['bad']
-    print(cosine_similarity(np.array(vec_dict.values())))
+    mostsimilar('good', vec_dict)
     # print('Cosine distance between `good` and `great`:', cosine_similarity(a,b))
     # print('Cosine distance between `good` and `bad`:', cosine_similarity(a,c))
